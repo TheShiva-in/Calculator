71bd65feacb6c19d6672333a13b9ded09d768730
shiva yadav <shivayadav11112003@gmail.com>
2026-07-20 21:22:00 +0530

    Add modulus operation and prompt for another calculation in calculator

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
+
+ char choice;
+
     float number1, number2,result;
     char choose;
     cout<<"Enter the first number:";
@@ -35,10 +39,24 @@ int main()
     cout<<result;
     break;
 
+    case'%':
+    if(number1!=0)
+    {
+    cout<< fmod(number1,number2);
+
+    break;
+    }
+
     default:
     cout<<"Invalid Operator!";
         break;
+
+
+    cout<<"Do you want to do another calculation? (y/n)";
+    cin>> choice;
     }
+
+
 
 
 }
